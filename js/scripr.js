7d5384261f22cc4f7c9a8f023edab7a7815edfb4
Bulat <bulich-19349@mail.ru>
2020-10-28 16:15:11 +0300

    5

--- js/scripr.js
+++ js/scripr.js
@@ -1,58 +1,72 @@
 "use strict"; // Поддерживает только новые форматы и делает строгим режимом;
-let musicTrec;
-
-musicTrec = +prompt("Сколько песен прослушал?", "");
 
 const musicPrefers = {
+    count: 0,
     genre: [],
     singer: {},
     reliz: [],
-    privat: true
-};
-
-function qwastion()
-{
-    while (musicTrec == null || isNaN(musicTrec) || musicTrec == "")
-        musicTrec = +prompt("Сколько песен прослушал?", "");
-}
-
-function anketa()
-{
-    for (let i = 0; i < 2; i++)
-    {
-        let a = prompt("Твой любимый исполнитель", "");
-        let b = prompt("Его лучшая песня", "");
-        if (a != null && b != null && a !='' && b != '' && a.length < 50 && b.length < 50)
+    privat: true,
+    qwastion: function()
         {
-            musicPrefers.singer[a] = b;
-            console.log("ok");
-        }   
+        while (musicPrefers.Count == null || isNaN(musicPrefers.Count) || musicPrefers.Count == "")
+            musicPrefers.Count = +prompt("Сколько песен прослушал?", "");
+        },
+    anketa: function()
+    {
+        for (let i = 0; i < 2; i++)
+        {
+            let a = prompt("Твой любимый исполнитель", "");
+            let b = prompt("Его лучшая песня", "");
+            if (a != null && b != null && a !='' && b != '' && a.length < 50 && b.length < 50)
+            {
+                musicPrefers.singer[a] = b;
+                console.log("ok");
+            }   
+            else
+            {
+                i--;
+                console.log("error");
+            }
+        }
+    },
+    levelMusic: function()
+    {
+        if(musicPrefers.Count <= 5)
+        console.log("Слушай радио");
         else
         {
-            i--;
-            console.log("error");
+            console.log("Молодец");
         }
-    }
-}
-
-function levelMusic()
-{
-    if(musicTrec <= 5)
-    console.log("Слушай радио");
-    else
+    },
+    checked: function()
     {
-        console.log("Молодец");
+        if(hidden == false)
+            console.log(musicPrefers);
+    },
+    reversePrivate: function()
+    {
+        if (musicPrefers.privat == false)
+            musicPrefers.privat = true;
+        else
+            musicPrefers.privat = false;
+    },
+    youLoveGenre: function()
+    {
+        for(let i = 1; i <= 3; i++)
+        {
+            let genre;
+            genre = prompt(`Твой любимый жанр ${i}`,"");
+            if(genre == null || genre == "")
+                i--;
+            else
+                musicPrefers.genre[i - 1] = genre.toLowerCase();
+        }
+        // musicPrefers.genre.pop();
+         musicPrefers.genre.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+         })
     }
-}
-
-function checked(hidden)
-{
-    if(hidden == false)
-        console.log(musicPrefers);
-}
+};
+// checked(musicPrefers.privat);
 
 
-// qwastion();
-// anketa();
-levelMusic();
-checked(musicPrefers.privat);
