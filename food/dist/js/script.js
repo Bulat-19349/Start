f90009490fb356cff7317d4af5ad37c305bdd4b2
Bulat <bulich-19349@mail.ru>
2020-11-01 16:59:12 +0300

    tab, timer, modal, class

--- food/dist/js/script.js
+++ food/dist/js/script.js
@@ -129,15 +129,16 @@ function getZero(num){
 
         window.addEventListener('scroll', showOpenModal);
 
-        //Использование классов для кааточек
+        //Использование классов для карточек
 
         class MenuCard{
-            constructor(src, alt, title, disc, price,parentSelector){
+            constructor(src, alt, title, disc, price,parentSelector, ...classes){
                 this.src = src;
                 this.alt = alt;
                 this.title = title;
                 this.disc = disc;
                 this.price = price;
+                this.classes = classes;
                 this.parent = document.querySelector(parentSelector);
                 this.transfer = 80;
                 this.changeTorRu();
@@ -147,8 +148,14 @@ function getZero(num){
             }
             render(){
                 let element = document.createElement('div');
+                if (this.classes.length === 0){
+                    this.element = 'menu__item';
+                    element.classList.add(this.element);
+                }
+                this.classes.forEach(item =>{
+                    element.classList.add(item);
+                })
                 element.innerHTML = `
-                <div class="menu__item">
                     <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.disc}</div>
@@ -157,9 +164,9 @@ function getZero(num){
                         <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                </div>
                 `;
                 this.parent.append(element);
+                console.log(this.classes);
             }
         }
         let div = new MenuCard(
@@ -168,7 +175,7 @@ function getZero(num){
             'Меню "Фитнес"',
             'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
             "10",
-            ".menu .container"
+            ".menu .container",
         );
         div.render();
 
@@ -178,7 +185,8 @@ function getZero(num){
             'Меню “Премиум”',
             'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
             15,
-            ".menu .container"
+            ".menu .container",
+            "menu__item"
         ).render();
 
         new MenuCard(
@@ -187,10 +195,11 @@ function getZero(num){
             'Меню "Постное"',
             'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
             20,
-            ".menu .container"
+            ".menu .container",
+            "menu__item"
         ).render();
 
-
+  
 
     hideContent();
     showContent();
